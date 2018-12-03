9f7e3ce26d6e50c5b76bf4c412f5ac12a4e690e3
Ding Tao <miyatsu@qq.com>
2018-12-03 13:54:31 +0000

    ch: user: Fix username can not login issue
    Fix issue #3
    
    Signed-off-by: Ding Tao <miyatsu@qq.com>

--- package/user/install.sh
+++ package/user/install.sh
@@ -14,30 +14,21 @@
 username=chinfo
 password=chinfo
 
-# Add user with ${username} and ${password}
-#
-# No.1 echo: user password
-# No.2 echo: user password retype
-# No.3 echo: Full Name []:
-# No.4 echo: Room Number []:
-# No.5 echo: Work Phone []:
-# No.6 echo: Home Phone []:
-# No.7 echo: Other []:
-# No.8 echo: Is the information correct? [Y/n]
-(echo ${password};
-echo ${password};
-echo ${username};
-echo '1406';
-echo '0731-12345678'
-echo '13888888888'
-echo 'empty') | useradd -m ${username}
-
+# Add user
+useradd -m -s /bin/bash ${username}
 if [ $? -ne 0 ]
 then
 	echo "'useradd' exit none zero!"
 	exit 1
 fi
 
+# Change password
+echo "${username}:${password}" | chpasswd
+if [ $? -ne 0 ]
+then
+	echo "'chpasswd' exit none zero!"
+	exit 1
+fi
 
 # Add sudo privilege
 echo "${username} ALL=(ALL) ALL" >> /etc/shdoers
